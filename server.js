6097da5fb98dacb136c00c9c3ba68f7e3119304b
alan <alan.paulo@radixeng.com.br>
2020-05-11 23:25:19 -0300

    updating response

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => console.log('running'));
+app.get('/', (req, res) => res.send('running'));
 
 app.post('/signin',(req, res) => signIn.handleSignIn(req, res, db, bcrypt));
 
