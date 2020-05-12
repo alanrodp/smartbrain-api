d9076612f053e90dfc9ab1b677cd140a5d2e7d76
alan <alan.paulo@radixeng.com.br>
2020-05-11 23:57:36 -0300

    Adding database connection

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'user',
-        password: '',
-        database: 'smartbrain'
+        connectString: 'process.env.DATABASE_URL',
+        ssl: true
     }
 })
 
